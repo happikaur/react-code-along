9f6f07996ff2b345e4cd05addee64ee917d3eb9a
Happi Kaur <38865414+happikaur@users.noreply.github.com>
2022-02-07 11:47:31 +0000

    code along and intro to react

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.scss';
+import Header from './components/Header/Header'
+
+
+const App = () => {
+  // let message = "We can do JS here!";
+
+  // if (message.length > 10) {
+  //   message += " , we add logic here as well!"
+  // }
+
+ 
+
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
+}
+
+export default App;
